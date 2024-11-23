655f214cfeca471ccee1348e4301a59342d1067f
Yasar9108 <1EP21IS121.yasarr@gmail.com>
2024-11-23 05:49:48 +0530

    Third Commit

---
+++ tnsf_/src/Basics_Java/DataTypes.java
@@ -0,0 +1,28 @@
+package Basics_Java;
+class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10;
+		float b =1000.81f;
+		double c =200.10d;
+		byte d =10;
+		long e=10000l;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(e);
+		int a1 =10;
+		int b2 =29;
+		int c3 =30;
+		System.out.println("The sum of Three Numbers is :"+a1+b2+c3);
+//		Practise q 1 on datatypes
+		System.out.print("Marks in 3 Subjects :");
+		float social =80.10f;
+		float Science =90.10f;
+		float maths =95.0f;
+		System.out.println("The Cgpa in Three subjects is :"+(social+Science+maths)/300.0f*100);
+	
+		
+	}
+}
